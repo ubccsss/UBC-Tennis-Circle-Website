25698bc547b1bf3ea88ed49981f8a2595518b32f
Elliot Saha <elliotsaha@gmail.com>
2023-11-11 20:51:27 -0800

    feat(lib/mongoose.ts): created connectToDb function for mongoDB, closes #12

--- src/lib/winston.ts
+++ src/lib/winston.ts
@@ -1,30 +1,27 @@
-// Winston
 import winston from "winston";
-
-// Path
 import path from "path";
 
-
-const logger = winston.createLogger({
-  level: 'info',
+export const logger = winston.createLogger({
+  level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.File({
-        // puts error logs in logs file 
-        filename: path.resolve("src/logs/error.log"),
-        level: 'error' }),
+      // puts error logs in logs file
+      filename: path.resolve("src/logs/error.log"),
+      level: "error",
+    }),
     new winston.transports.File({
-        // puts combined logs in logs file
-        filename: path.resolve("src/logs/combined.log") }),
+      // puts combined logs in logs file
+      filename: path.resolve("src/logs/combined.log"),
+    }),
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
 }
-
-
-export default logger
