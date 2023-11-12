0b5652587623f581f45cff3d5f4fb5d4124245dc
BRIAN V <brianvalentinoadhitya@gmail.com>
2023-11-11 21:12:58 -0800

    fix(providers/index.tsx): added exports, fixed bugs and imports #14

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from "next";
-import { Chakra } from "@providers/chakra";
+import { Chakra } from "@providers/index";
+import { ReactQueryProvider } from "@providers/index";
 
 export const metadata: Metadata = {
   title: "UBC Tennis Circle",
   description: "A UBC organization",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <html lang="en">
       <body>
         <Chakra>
+          <ReactQueryProvider>
           {children}
+          </ReactQueryProvider>
         </Chakra>
       </body>
     </html>
   );
-};
 
 export default RootLayout;
