13f9cc0c5eb57b245458b18e9d079b81f2cef09d
Elliot Saha <elliotsaha@gmail.com>
2024-01-29 19:27:46 -0800

    fix(style): ran npm run fix

--- src/app/(pages)/faq/page.tsx
+++ src/app/(pages)/faq/page.tsx
@@ -1,116 +1,141 @@
-
-import { Text, Container, Center, VStack, Heading, Box, Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel } from "@chakra-ui/react";
-import Link from "next/link";
-
+import {
+  Text,
+  Container,
+  Center,
+  VStack,
+  Heading,
+  Box,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+} from '@chakra-ui/react';
+import Link from 'next/link';
 
 const faq = () => {
   return (
-    <Container maxW="container.xl" py={{ base: '32', lg: '20' }}>
+    <Container maxW="container.xl" py={{base: '32', lg: '20'}}>
       <Center>
         <VStack
           align="center"
           justify="center"
-          w={{ base: '100%', sm: 'max-content' }}
+          w={{base: '100%', sm: 'max-content'}}
         >
-          <Heading as="h1" size="xl" mb={4}>What can we help you find?</Heading>
-          <Box width={{ base: "100%", sm: "50%" }} mb={7}>
+          <Heading as="h1" size="xl" mb={4}>
+            What can we help you find?
+          </Heading>
+          <Box width={{base: '100%', sm: '50%'}} mb={7}>
             <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </Text>
           </Box>
-          <Box width={{ base: "100%", sm: "75%" }}>
+          <Box width={{base: '100%', sm: '75%'}}>
             <Accordion defaultIndex={[0]} allowMultiple>
               <AccordionItem>
                 <h2>
                   <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left' >
-                      <Heading size="md" pt={4} pb={4}>Section 1 title</Heading>
+                    <Box as="span" flex="1" textAlign="left">
+                      <Heading size="md" pt={4} pb={4}>
+                        Section 1 title
+                      </Heading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
               </AccordionItem>
 
               <AccordionItem>
                 <h2>
                   <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      <Heading size="md" pt={4} pb={4}>Section 2 title</Heading>
+                    <Box as="span" flex="1" textAlign="left">
+                      <Heading size="md" pt={4} pb={4}>
+                        Section 2 title
+                      </Heading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
               </AccordionItem>
               <AccordionItem>
                 <h2>
                   <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      <Heading size="md" pt={4} pb={4}>Section 3 title</Heading>
+                    <Box as="span" flex="1" textAlign="left">
+                      <Heading size="md" pt={4} pb={4}>
+                        Section 3 title
+                      </Heading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
               </AccordionItem>
               <AccordionItem>
                 <h2>
                   <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      <Heading size="md" pt={4} pb={4}>Section 4 title</Heading>
+                    <Box as="span" flex="1" textAlign="left">
+                      <Heading size="md" pt={4} pb={4}>
+                        Section 4 title
+                      </Heading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
               </AccordionItem>
               <AccordionItem>
                 <h2>
                   <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      <Heading size="md" pt={4} pb={4}>Section 5 title</Heading>
+                    <Box as="span" flex="1" textAlign="left">
+                      <Heading size="md" pt={4} pb={4}>
+                        Section 5 title
+                      </Heading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
               </AccordionItem>
             </Accordion>
           </Box>
-          <Heading size="sm" pt={10}>Don't find what you need?</Heading>
+          <Heading size="sm" pt={10}>
+            Don't find what you need?
+          </Heading>
           <Link href="/contact">
-            <Heading size="xs" color="#4318FF">CONTACT US {'>'}</Heading>
+            <Heading size="xs" color="#4318FF">
+              CONTACT US {'>'}
+            </Heading>
           </Link>
-
         </VStack>
       </Center>
     </Container>
