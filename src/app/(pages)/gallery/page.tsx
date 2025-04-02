d49591b2fbf95465e7221dcdcae188563259ce51
Elliot Saha <elliotsaha@gmail.com>
2025-04-01 17:28:56 -0700

    fix(gallery): fixed gallery rendering (temp)

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -16,22 +16,9 @@ import {
 import axios from "axios";
 import { FiAlertCircle, FiInstagram } from "react-icons/fi";
 import { useQuery } from "@tanstack/react-query";
-
-const getPosts = async () => {
-  const posts = await axios.get(
-    `${process.env.NEXT_PUBLIC_HOSTNAME}/api/instagram/posts`,
-  );
-  return posts.data;
-};
+import data from "@/constants/instagram.json"
 
 const Gallery = () => {
-  const { isPending, error, data } = useQuery<
-    Array<{ media_url: string; permalink: string; caption: string }>
-  >({
-    queryKey: ["instagram-posts"],
-    queryFn: getPosts,
-  });
-
   return (
     <Container maxW="container.xl">
       <Box mt="8">
@@ -53,32 +40,7 @@ const Gallery = () => {
         alignItems="center"
         flexDir="column"
       >
-        {error && (
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            textAlign="center"
-            gap="2"
-            w="100%"
-          >
-            <Icon as={FiAlertCircle} color="red.400" fontSize="28" />
-            <Text maxW="64">An unexpected error has occurred</Text>
-          </Flex>
-        )}
         <SimpleGrid columns={{ base: 1, sm: 2, lg: 3 }} gap="6">
-          {isPending &&
-            new Array(9)
-              .fill(null)
-              .map((_, index) => (
-                <Skeleton
-                  key={index}
-                  borderRadius="8"
-                  w={{ base: "100%", md: "72" }}
-                  h={{ base: "100%", md: "72" }}
-                />
-              ))}
-
           {data &&
             data.map((post, index) => (
               <Box
@@ -89,12 +51,12 @@ const Gallery = () => {
                 borderRadius="8"
               >
                 <a
-                  href={post.permalink}
+                  href={post.url}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <Image
-                    src={post.media_url}
+                    src={post.displayUrl}
                     alt={post.caption}
                     objectFit="cover"
                     width="100%"
